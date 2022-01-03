0c447d1213b93c45f71bfb43a903ea472c08d8d2
Milon Biswas <84205020+milonbiswas4702@users.noreply.github.com>
2022-01-03 21:35:05 +0600

    Add files via upload

---
+++ changelog.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cat > CHANGELOG.new << EOF
+## "${2}"
+
+$(git log "$1..HEAD"  --oneline | grep -v Merge | sed -e "s/\[\(FAB-[0-9]*\)\]/\[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/ \(FAB-[0-9]*\)/ \[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/\([0-9|a-z]*\)/* \[\1\](https:\/\/github.com\/hyperledger\/fabric-samples\/commit\/\1)/")
+
+EOF
+cat CHANGELOG.md >> CHANGELOG.new
+mv -f CHANGELOG.new CHANGELOG.md
